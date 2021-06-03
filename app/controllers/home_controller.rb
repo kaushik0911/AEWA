53a4d566e8c659beb86aa06808fce1d0f8e7a76d
Kaushik Shamantha <kaushikshamantha@gmail.com>
2021-06-04 03:43:59 +0530

    flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,29 @@
 class HomeController < ApplicationController
-  def index
-    @sliders  = Carousel.show
-    @services = Service.show
-    @team_members = TeamMember.show
-  end
+
+  before_action :init
+
+  def index; end
 
   def contact_us
-    contact_us = ContactU.new(contact_us_params)
-    if verify_recaptcha(model: contact_us)
-      flash[:notice] = "You message have successfully sent."
-      redirect_to :root
+    @contact_us.assign_attributes(contact_us_params)
+    if verify_recaptcha(model: @contact_us) && @contact_us.save
+      flash[:success] = t(".success")
     else
-      render 'index'
+      flash[:danger] = t(".danger")
     end
+    redirect_to :root
   end
 
   private
 
   def contact_us_params
     params.permit(:name, :email, :subject, :comment)
+  end
+
+  def init
+    @sliders  = Carousel.show
+    @services = Service.show
+    @team_members = TeamMember.show
+    @contact_us = ContactU.new
   end
 end
