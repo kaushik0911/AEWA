40643cf746d8613e2f930b6369b3b572e596cdbd
Kaushik Shamantha <kaushikshamantha@gmail.com>
2021-06-04 20:41:14 +0530

    slider seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,16 @@
-AdminUser.create!(email: 'admin@example.com', password: 'pa$$w0rd', password_confirmation: 'pa$$w0rd')
-ProjectStatus.create([{name: 'Pending'}, {name: 'Negotiating'}, {name: 'Accepted'}, {name: 'Inprogress'}, {name: 'Hold'}, {name: 'Completed'}, {name: 'Canceled'}])
+require 'open-uri'
+
+# AdminUser.create!(email: 'admin@example.com', password: 'pa$$w0rd', password_confirmation: 'pa$$w0rd')
+# ProjectStatus.create([{name: 'Pending'}, {name: 'Negotiating'}, {name: 'Accepted'}, {name: 'Inprogress'}, {name: 'Hold'}, {name: 'Completed'}, {name: 'Canceled'}])
+
+for i in 0...5
+  carousel = Carousel.create({
+    title: "Where can I get some?",
+    description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    url: "https://www.lipsum.com/",
+    display: true
+  })
+
+  downloaded_image = open("https://picsum.photos/1500/800")
+  carousel.image.attach(io: downloaded_image, filename: "image.jpg")
+end
