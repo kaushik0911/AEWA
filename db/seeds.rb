1d1c61a34b4a0e150591672a891cf1930d89425a
Kaushik Shamantha <kaushikshamantha@gmail.com>
2021-06-04 21:08:43 +0530

    seed for application

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,20 @@
 require 'open-uri'
 
-# AdminUser.create!(email: 'admin@example.com', password: 'pa$$w0rd', password_confirmation: 'pa$$w0rd')
-# ProjectStatus.create([{name: 'Pending'}, {name: 'Negotiating'}, {name: 'Accepted'}, {name: 'Inprogress'}, {name: 'Hold'}, {name: 'Completed'}, {name: 'Canceled'}])
+AdminUser.create!(email: 'admin@example.com', password: 'pa$$w0rd', password_confirmation: 'pa$$w0rd')
 
-# for i in 0...5
-#   carousel = Carousel.create({
-#     title: "Where can I get some?",
-#     description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
-#     url: "https://www.lipsum.com/",
-#     display: true
-#   })
+ProjectStatus.create([{name: 'Pending'}, {name: 'Negotiating'}, {name: 'Accepted'}, {name: 'Inprogress'}, {name: 'Hold'}, {name: 'Completed'}, {name: 'Canceled'}])
 
-#   downloaded_image = open("https://picsum.photos/1500/800")
-#   carousel.image.attach(io: downloaded_image, filename: "image.jpg")
-# end
+for i in 0...5
+  carousel = Carousel.create({
+    title: "Where can I get some?",
+    description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    url: "https://www.lipsum.com/",
+    display: true
+  })
+
+  downloaded_image = open("https://picsum.photos/1500/800")
+  carousel.image.attach(io: downloaded_image, filename: "image.jpg")
+end
 
 for i in 0...4
   team_member = TeamMember.create({
@@ -29,4 +30,15 @@ for i in 0...4
 
   downloaded_image = open("https://picsum.photos/340/250")
   team_member.image.attach(io: downloaded_image, filename: "image.jpg")
+end
+
+for i in 0...9
+  service = Service.create({
+    title: "Lorem Ipsum",
+    description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+    display: true
+  })
+
+  downloaded_image = open("https://picsum.photos/450/200")
+  service.image.attach(io: downloaded_image, filename: "image.jpg")
 end
